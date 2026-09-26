5fc629d45562bab646f5de5312ccd9e6f3b42a8c
Tùng Lâm Automation <183681608+Tunglam0605@users.noreply.github.com>
2026-09-26 15:47:33 +0700

    release: bump version to 0.24.3

--- b300_version.py
+++ b300_version.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-__version__ = "0.24.2"
+__version__ = "0.24.3"
 def main(argv=None) -> int:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--check", metavar="VERSION")
